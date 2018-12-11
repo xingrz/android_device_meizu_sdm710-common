4762da151b7e5f0bcb1830cd40e72468f6dfc2f5
GuaiYiHu <guaiyihu@foxmail.com>
2018-12-11 23:18:56 +0800

    sdm710: proprietary: Update Radio blobs
    * Update ims library symlinks
    * Update props for new radio blobs
    * Uprev radio and ims version
    
    Change-Id: Ib06c9682907917c875884b2f1963002e9622663c

--- common.mk
+++ common.mk
@@ -211,6 +211,9 @@ PRODUCT_COPY_FILES += \
 
 # Radio
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.2 \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0 \
     ims-ext-common \
     libjson \
     librmnetctl \
