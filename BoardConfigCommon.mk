2cbb4d5e91be22ace5709096ccb53c17a8814de1
XiNGRZ <chenxingyu92@gmail.com>
2019-10-19 17:27:10 +0800

    sdm710: Add shims for camera vstab blobs
    Change-Id: I99f10ee645ca58d101a80a5cf8e22fd98bf9b36b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -60,6 +60,8 @@ USE_XML_AUDIO_POLICY_CONF := 1
 USE_DEVICE_SPECIFIC_CAMERA := true
 USE_CAMERA_STUB := true
 BOARD_QTI_CAMERA_32BIT_ONLY := true
+TARGET_LD_SHIM_LIBS += /vendor/lib/libmms_hal_vstab.so|/vendor/lib/libshim_camera.so
+TARGET_LD_SHIM_LIBS += /vendor/lib/libmms_warper_vstab.so|/vendor/lib/libshim_camera.so
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
--- common.mk
+++ common.mk
@@ -96,6 +96,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    libshim_camera \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
