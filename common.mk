608147b03c0550e9156ad0d3ab650e4e646ff1ff
XiNGRZ <chenxingyu92@gmail.com>
2019-12-27 15:29:55 +0800

    sdm710: Implemented custom Light HAL
    Change-Id: I16f6225a0317f2ac3dda2c9940c727faf3410dfc

--- common.mk
+++ common.mk
@@ -178,6 +178,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.meizu_sdm710
+
 # Media
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/media,$(TARGET_COPY_OUT_VENDOR)/etc) \
