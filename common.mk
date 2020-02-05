02bbe161b5693c1e6eaf15f04e49a4946cc9f5d3
XiNGRZ <chenxingyu92@gmail.com>
2020-02-05 21:42:50 +0800

    sdm710: Use prebuilt audio HAL
    Change-Id: Ifa12400be64b7fa5cf139cd14bf88ad866c70c9d

--- common.mk
+++ common.mk
@@ -68,14 +68,12 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
-    audio.primary.sdm710 \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
     libdynproc \
     libtinyalsa \
-    libtinycompress \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
